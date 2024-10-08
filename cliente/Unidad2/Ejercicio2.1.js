525a436e3e4ad88b8cbf750b8d8ed9f20007c9b7
Lucas Seiz Valdes <czd22983@educastur.es>
2024-10-08 11:13:32 +0200

    como

--- cliente/Unidad2/Ejercicio2.1.js
+++ cliente/Unidad2/Ejercicio2.1.js
@@ -1,7 +1,11 @@
 function Ejercicio2() {
+    /*Haz un programa que muestre en una ventana el mensaje “Hola mundo JS”
+    */
     document.write("Hola mundo JS");
     }
     function Ejercicio2_2() {
+         /*Ejercicio de clase de notas
+    */
         let nombre;
         let nota;
         nombre=prompt('Escriba su nombre: ', '');
@@ -39,8 +43,62 @@ function Ejercicio2() {
         if(pro>=9){
             alert('Sobresaliente!!');
         }
-        else(pro>=7){
+        else(pro>=7);{
             alert('Notable!!');
             
         }
+    }
+    function Ejercicio2_5() {
+        /*Haz un programa que nos pida un número, y nos muestre en una única pantalla, el doble, el triple
+        y el cuádruple del número que habíamos introducido (cada uno en una línea).*/
+        let num1="10", num2="0";
+       
+        res=num1/num2;
+        
+        alert('El resultado es: '+ res);
+        //El resultado es infinito
+        
+    }
+    function Ejercicio2_6() {
+        /*Haz un programa que nos pregunte nuestro nombre, nuestra edad. El programa nos tiene que dar
+como resultado los días que hemos vivido hasta el momento (suponiendo años de 365 días).
+*/
+    let nombre, edad; 
+    nombre=prompt('Introduce tu nombre: ', "");
+    edad=prompt('Introduce tu edad: ', "");
+
+    alert(nombre + ' ha vivido '+edad*365 + ' dias.');  
+    }
+
+    function Ejercicio2_7() {
+       /* Haz un programa que nos pida un número, y nos muestre en una única pantalla, el doble, el triple
+y el cuádruple del número que habíamos introducido (cada uno en una línea)*/
+        let num;
+        num=prompt('Introduce un número: ');
+
+        document.write('El doble es: ' +num*2, "</br>");
+        document.write('El triple es: ' +num*3, "</br>");
+        document.write('El cuádruple es: ' +num*4, "</br>");
+
+        //en una sola linea document.write('El doble es: '+num*2, "</br>", 'El triple es: '+num*3, "</br>", 'El cuádruple es: '+num*4, "</br>");
+        
+    }
+    function Ejercicio2_8 () {
+        /*El siguiente programa tiene errores. Corrígelo para que funcione y explica qué hace.
+        <html>
+        <script>
+        /* Programa 6.htm
+        var a, b;
+        a= prompt(“Escribe la base:”)
+        b= prompt(“Escribe la altura:”)
+        alert(“Area= “ +(a*b/2);
+        </script>
+        </html>
+
+         */
+        let a, b;
+        a= prompt('Escribe la base:');
+        b= prompt('Escribe la altura:');
+        alert('Area= ' + (a*b/2));
+        
     }
---
+++ cliente/suma.js
@@ -0,0 +1,11 @@
+/**
+ * @summary Calcula la suma de dos números
+ * @description Esta función tan compleja calcula la suma de dos números
+ * @param {number} x Primer número a sumar
+ * @param {number} y Segundo número a sumar
+ * @returns {number} Devuelve la suma de los dos números
+ */
+
+function suma(a,b){
+    return a + b;
+}
